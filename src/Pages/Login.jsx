260d84e93ced46676f6a00074f2afacf6b4006b6
Shubham10320 <chaubeyshubh1032@gmail.com>
2023-10-22 00:35:29 +0530

    login signUp design and responsiveness done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Box, Button, Flex, Heading, Image, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import lock from '../assets/lock.png'
+import Navbar from '../Components/Navbar'
+import { ViewIcon , ViewOffIcon, SpinnerIcon, ArrowForwardIcon, PhoneIcon, LockIcon} from '@chakra-ui/icons'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate=useNavigate()
+
+
   return (
-    <div>Login</div>
+    <>
+    <Navbar/>
+    <Box w={{base:"90%",sm:"70%" ,md:"40%",lg:"30%",xl:"30%"}} m="auto" boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" mt="50px" p="30px">
+
+       <Flex w={{base:"50%",sm:"50%" ,md:"50%",lg:"50%",xl:"32%"}} m="auto" alignItems="center" justifyContent="center" gap="10px" border="2px solid teal" p="5px" borderRadius="30px">
+        <LockIcon fontSize="25px" color="teal"/>
+        <Text color="teal" fontWeight="bold" fontSize="20px">Sign Up</Text>
+       </Flex>
+
+       <form>
+          <Input type="text" mt="50px" mb="15px"  placeholder='Email Address' variant='flushed'/>
+          <InputGroup mb="30px">
+            <Input type="text"  placeholder='Password' variant='flushed'/>
+            <InputRightElement>
+              <ViewIcon fontSize="25px" color="teal"/>
+            </InputRightElement>
+          </InputGroup>
+          <Button w="100%" mt="20px" leftIcon={<ArrowForwardIcon />} colorScheme='teal' variant='solid'>Login</Button>
+        </form>       
+
+
+    </Box>
+    </>
+    
   )
 }
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Image, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import men from '../assets/men.png'
+import Navbar from '../Components/Navbar'
+import { ViewIcon , ViewOffIcon, SpinnerIcon, ArrowForwardIcon, PhoneIcon} from '@chakra-ui/icons'
+import { Link, useNavigate } from 'react-router-dom'
+
 const Register = () => {
+  const navigate=useNavigate()
+
+
   return (
-    <Box w="30%" m="auto" border="1px solid black" mt="100px" textAlign="center">
-        <Heading as="h1" fontSize="35px">Register</Heading>
-        <form>
-          
-        </form>
+    <>
+    <Navbar/>
+    <Box  w={{base:"90%",sm:"70%" ,md:"45%",lg:"38%",xl:"30%"}}  m="auto" boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" mt="50px" p="30px">
+
+       <Flex w={{base:"67%",sm:"50%" ,md:"50%",lg:"60%",xl:"35%"}} m="auto" alignItems="center" justifyContent="center" gap="10px" border="2px solid teal" p="5px" borderRadius="30px">
+        <Image src={men} w="35px"/>
+        <Text color="teal" fontWeight="bold" fontSize="20px">Sign Up</Text>
+       </Flex>
+
+       <form>
+          <Input type="text" mt="20px" mb="15px"  placeholder='Username' variant='flushed'/>
+          <InputGroup mb="30px">
+            <Input type="text"  placeholder='Mobile Number' variant='flushed'/>
+            <InputRightElement>
+              <PhoneIcon fontSize="20px" color="teal"/>
+            </InputRightElement>
+          </InputGroup>
+          <Input type="text" mb="15px"  placeholder='Email Address' variant='flushed'/>
+          <InputGroup mb="30px">
+            <Input type="text"  placeholder='Password' variant='flushed'/>
+            <InputRightElement>
+              <ViewIcon fontSize="25px" color="teal"/>
+            </InputRightElement>
+          </InputGroup>
+          <Button w="100%" leftIcon={<ArrowForwardIcon />} colorScheme='teal' variant='solid'>Register</Button>
+          <Text>Forgot Password?</Text>
+          <Text>Do you have an account ? <Link to="/login"><span style={{color:"red"}}>Sign In</span></Link></Text>
+        </form>       
+
+
     </Box>
+    </>
+    
   )
 }
 
